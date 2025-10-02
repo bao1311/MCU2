556506e47c0e2e540eff22d88470432f33d70ba2
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-10-02 03:34:47 -0700

    Set priority Grouping for NVIC

--- USART2_Test/Core/Src/msp.c
+++ USART2_Test/Core/Src/msp.c
@@ -5,14 +5,14 @@
  *      Author: gphi1
  */
 
-
+#include "stm32f4xx_hal.h"
 void HAL_MspInit(void)
 {
 	// This file will do the low level processor specific inits
 
 	// 1. Set up the priority grouping of arm cortex mx processor
-
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 	// 2. Enable the required system exceptions of the arm cortex mx processor
 
-	// 3. Configure the priority for the system exceptions
+	// 3. Configure the pri:ority for the system exceptions
 }
