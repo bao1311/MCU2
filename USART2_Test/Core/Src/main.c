82899c5bbfad78145205f2245c22a253913b200d
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-10-02 23:15:39 -0700

    USART2 Low level configuration

--- USART2_Test/Core/Src/main.c
+++ USART2_Test/Core/Src/main.c
@@ -18,8 +18,41 @@ int main(void)
 	return 0;
 }
 
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+
+	/*
+	 * Low level configuration
+	 */
+
+	// Enable USART2 Clock
+	__HAL_RCC_USART2_CLK_ENABLE();
+	// Pin Configurations
+	GPIO_InitTypeDef gpio;
+	gpio.Alternate = GPIO_AF7_USART2;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull = GPIO_PULLUP;
+	gpio.Speed = GPIO_SPEED_FAST;
+
+	// USART2_TX -> PA2
+	gpio.Pin = GPIO_PIN_2;
+	HAL_GPIO_Init(GPIOA, &gpio);
+
+	// USART2_TX -> PA3
+	gpio.Pin = GPIO_PIN_3;
+	HAL_GPIO_Init(GPIOA, &gpio);
+
+	// NVIC Configuration Enable Interrupt
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+
+	// Interrupt priority set
+	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+
+}
 void UART2_Init(void)
 {
+
+	// High level configuration
 	huart2.Instance = USART2;
 	huart2.Init.BaudRate = 115200;
 	huart2.Init.WordLength = UART_WORDLENGTH_8B;
