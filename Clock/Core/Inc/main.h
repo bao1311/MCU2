02ab43d1279b70bcf4ee0f7737dfac47271be8c3
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-10-12 21:12:56 +0700

    fixing clock bug

--- Clock/Core/Inc/main.h
+++ Clock/Core/Inc/main.h
@@ -28,6 +28,8 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_rcc.h"
+#include "stm32f4xx_hal_rcc_ex.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
