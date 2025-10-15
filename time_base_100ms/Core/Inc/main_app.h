1b5765d7deb4ceae6de7c42389a645d942f82456
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-10-15 16:58:06 +0700

    Initial commit for time_base_100

---
+++ time_base_100ms/Core/Inc/main_app.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: Oct 15, 2025
+ *      Author: gphi1
+ */
+
+#ifndef INC_MAIN_APP_H_
+#define INC_MAIN_APP_H_
+
+
+
+#endif /* INC_MAIN_APP_H_ */
---
+++ time_base_100ms/Core/Src/main_app.c
@@ -0,0 +1,15 @@
+/*
+ * main_app.c
+ *
+ *  Created on: Oct 15, 2025
+ *      Author: gphi1
+ */
+#include "main_app.h"
+
+int main()
+{
+	HAL_I
+
+	return 0;
+}
+
