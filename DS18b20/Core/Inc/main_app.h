a6151fc33169dd60414c5858de7877d5b3ce1a47
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-10-31 20:38:37 +0700

    Initialize the GPIO for one wire protocol

--- DS18b20/Core/Inc/main_app.h
+++ DS18b20/Core/Inc/main_app.h
@@ -8,6 +8,7 @@
 #ifndef INC_MAIN_APP_H_
 #define INC_MAIN_APP_H_
 
-
+#include "stm32f407xx.h"
+#include "stm32f4xx_hal_gpio.h"
 
 #endif /* INC_MAIN_APP_H_ */
--- DS18b20/Core/Src/main_app.c
+++ DS18b20/Core/Src/main_app.c
@@ -5,5 +5,50 @@
  *      Author: gphi1
  */
 #include "main_app.h"
+#include <stdio.h>
+void GPIO_Init();
+void DS18B20_ReadTemp();
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+	// GPIO Init for the DS18b20 on STM32 discovery board
+	GPIO_Init();
 
+	while (1)
+	{
+		float temp = DS18B20_ReadTemp();
+		printf("Current temperature is: %.2f Celsius\r\n",temp);
+		delay();
+	}
+
+
+}
+
+
+void DS18B20_ReadTemp()
+{
+	// Data is transferred in LSB mode
+
+}
+
+void GPIO_Init()
+{
+	// Use PA1 for the one wire protocol
+	// Config the GPIOA Clock
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio;
+
+	/*
+	 * GPIO Info:
+	 * Port: A
+	 * Pin: 1
+	 */
+	gpio.Mode = GPIO_MODE_OUTPUT_OD;
+	gpio.Pin = GPIO_PIN_1;
+
+	// Init
+	HAL_GPIO_Init(GPIOA, &gpio);
+}
 
