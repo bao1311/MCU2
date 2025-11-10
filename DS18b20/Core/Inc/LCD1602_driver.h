31aa93928609acef8b80b74ef02a67a892f40c8e
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-11-09 23:12:17 -0700

    Added LCD1602GPIO_Init

--- DS18b20/Core/Inc/LCD1602_driver.h
+++ DS18b20/Core/Inc/LCD1602_driver.h
@@ -8,6 +8,15 @@
 #ifndef INC_LCD1602_DRIVER_H_
 #define INC_LCD1602_DRIVER_H_
 
+/*
+ * Macros for LCD1602 Module
+ */
+// Instruction Set Macros
+
+/*
+ * API for LCD1602 Module Driver
+ */
+void LCD1602_GPIOInit();
 void LCD1602_Init();
 void LCD1602_SendCMD();
 void LCD1602_SendData();
