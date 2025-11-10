3450ac71982d5ae5e3981e5800b5578b19f6e1f6
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-11-10 03:09:31 -0700

    LCD1602_Init Driver API Implemented

--- DS18b20/Core/Src/LCD1602_driver.c
+++ DS18b20/Core/Src/LCD1602_driver.c
@@ -7,13 +7,55 @@
 
 #include "main_app.h"
 
+#define LCD1602_ADDR	0x4E
+
 extern I2C_HandleTypeDef hi2c1;
 
 
+/*
+ * ******************************************************
+ * API Prototype
+ * ******************************************************
+ */
 void I2C1Init();
 void LCD1602_Init();
 void LCD1602_SendCMD();
 void LCD1602_SendData();
+
+/*
+ * @fn					- LCD1602_SendCMD
+ * @brief				- API for sending Command over to LCD 1602
+ * @param				-
+ * @return				-
+ */
+void LCD1602_SendCMD(uint8_t cmd)
+{
+	// Extract upper and lower part of the command
+	uint8_t upper = cmd & 0xF0;
+	uint8_t lower = cmd & 0x0F;
+	// Store them in an array
+	uint8_t data[4] = {};
+	// Changing the Enable bit of the upper 4 bits data
+	data[0] = upper | 0x0C; // E = 1; RS,RW = 0
+	data[1] = upper | 0x08; // E = 0; RS,RW = 0
+	// Changing the Enable bit of the lower 4 bits data
+	data[2] = lower | 0x0C; // E = 1; RS,RW = 0
+	data[3] = lower | 0x08; // E = 0; RS,RW = 0
+
+	HAL_I2C_Master_Transmit(&hi2c1, LCD1602_ADDR, data, 4, HAL_MAX_DELAY);
+}
+
+/*
+ * @fn					- LCD1602_SendData
+ * @brief				- API for sending data over to LCD 1602
+ * @param				-
+ * @return				-
+ */
+void LCD1602_SendData(uint8_t data)
+{
+
+
+}
 
 
 /*
@@ -44,4 +86,48 @@ void I2C1Init()
 		Error_Handler();
 	}
 
+}
+
+/*
+ * @fn					- LCD1602_Init
+ * @brief				- This function complete the Initialization sequence of LCD1602
+ * @param				-
+ * @return				-
+ */
+void LCD1602_Init()
+{
+	// Wait > 15ms
+	HAL_Delay(40);
+	// Send 0x30
+	LCD1602_SendCMD(0x30);
+	// Wait > 4.1ms
+	HAL_Delay(5);
+	// Send 0x30
+	LCD1602_SendCMD(0x30);
+	// Wait time > 100us
+	delay_us(200);
+	// Send 0x30
+	LCD1602_SendCMD(0x30);
+	// Set interface to be 4 bits length
+	HAL_Delay(1);
+	LCD1602_SendCMD(0x20);
+
+	/*
+	 * Display settings
+	 */
+	// Function Set. DL = 0(4bit mode); N = 1(2 display line); F = 0(5x7 style)
+	HAL_Delay(1);
+	LCD1602_SendCMD(0x28);
+	// Display off
+	HAL_Delay(1);
+	LCD1602_SendCMD(0x08); // D = 1; C,B = 0;
+	// Screen clear
+	HAL_Delay(1);
+	LCD1602_SendCMD(0x01);
+	// Entry mode set => I/D = 1(Increment); S = 0(No shift)
+	HAL_Delay(3);
+	LCD1602_SendCMD(0x06);
+	// Display on
+	HAL_Delay(1);
+	LCD1602_SendCMD(0x0C); // D = 1; C,B = 0;
 }
